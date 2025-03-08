0c9e10cef0d9554ce2920deb25c7fa17ee1c27a4
Abby <k.abhiditya09@gmail.com>
2025-03-08 17:52:34 +0530

    Time: 21 ms (54.79%), Space: 51.4 MB (14.23%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -15,21 +15,21 @@ class Solution {
 		return dp[dp.length-1][dp[0].length-1];
     }
 
-    public static int LCS(String text1, String text2, int i, int j, int[][] dp) {
-		if(i==text1.length() || j==text2.length()) {
-			return 0;
-		}
-		if(dp[i][j]!=-1) {
-			return dp[i][j];
-		}
-		int ans=0; 
-		if(text1.charAt(i)==text2.charAt(j)) {
-			ans=1+LCS(text1,text2,i+1,j+1,dp);
-		}else {
-			int s1=LCS(text1, text2, i+1,j,dp);
-			int s2=LCS(text1, text2, i, j+1,dp);
-			ans=Math.max(s1, s2);
-		}
-		return dp[i][j]=ans;
-    }
+    // public static int LCS(String text1, String text2, int i, int j, int[][] dp) {
+	// 	if(i==text1.length() || j==text2.length()) {
+	// 		return 0;
+	// 	}
+	// 	if(dp[i][j]!=-1) {
+	// 		return dp[i][j];
+	// 	}
+	// 	int ans=0; 
+	// 	if(text1.charAt(i)==text2.charAt(j)) {
+	// 		ans=1+LCS(text1,text2,i+1,j+1,dp);
+	// 	}else {
+	// 		int s1=LCS(text1, text2, i+1,j,dp);
+	// 		int s2=LCS(text1, text2, i, j+1,dp);
+	// 		ans=Math.max(s1, s2);
+	// 	}
+	// 	return dp[i][j]=ans;
+    // }
 }
