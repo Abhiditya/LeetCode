6e7ce0c3b376bfea3fb31ef12cbbf07f4d3ef8b3
Abby <k.abhiditya09@gmail.com>
2025-03-19 16:23:35 +0530

    Time: 16 ms (86.08%), Space: 53.5 MB (54.3%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        int n=s.length();
-        List<String> ll= new ArrayList<>();
-        HashMap<String, Integer> hash= new HashMap<>();
-        for(int i=0; i<=n-10; i++){
-            hash.put(s.substring(i,i+10), hash.getOrDefault(s.substring(i,i+10), 0)+1);
-            if(hash.get(s.substring(i,i+10))>1 && !ll.contains(s.substring(i,i+10))){
-                ll.add(s.substring(i,i+10));
-            }
-        }
-        return ll;
-
-        //optimized method
-        // HashSet<String> check = new HashSet<>();
-        // HashSet<String> repeated = new HashSet<>();
-
-        // for (int i = 0; i <= s.length() - 10; i++) {
-        //     String str = s.substring(i, i + 10);
-        //     if (!check.add(str)) {
-        //         repeated.add(str);
+        // int n=s.length();
+        // List<String> ll= new ArrayList<>();
+        // HashMap<String, Integer> hash= new HashMap<>();
+        // for(int i=0; i<=n-10; i++){
+        //     hash.put(s.substring(i,i+10), hash.getOrDefault(s.substring(i,i+10), 0)+1);
+        //     if(hash.get(s.substring(i,i+10))>1 && !ll.contains(s.substring(i,i+10))){
+        //         ll.add(s.substring(i,i+10));
         //     }
         // }
-        // return new ArrayList<>(repeated);
+        // return ll;
+
+        //optimized method
+        HashSet<String> check = new HashSet<>();
+        HashSet<String> repeated = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String str = s.substring(i, i + 10);
+            if (!check.add(str)) {
+                repeated.add(str);
+            }
+        }
+        return new ArrayList<>(repeated);
     }
 }
