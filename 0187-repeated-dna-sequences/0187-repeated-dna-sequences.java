0afbf68685ab533b5c649539ce01f5b8c183f38d
Abby <k.abhiditya09@gmail.com>
2025-03-19 16:18:03 +0530

    Time: 34 ms (7.33%), Space: 54.1 MB (6.45%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -3,7 +3,7 @@ class Solution {
         int n=s.length();
         List<String> ll= new ArrayList<>();
         HashMap<String, Integer> hash= new HashMap<>();
-        for(int i=0; i<n-9; i++){
+        for(int i=0; i<=n-10; i++){
             hash.put(s.substring(i,i+10), hash.getOrDefault(s.substring(i,i+10), 0)+1);
             if(hash.get(s.substring(i,i+10))>1 && !ll.contains(s.substring(i,i+10))){
                 ll.add(s.substring(i,i+10));
