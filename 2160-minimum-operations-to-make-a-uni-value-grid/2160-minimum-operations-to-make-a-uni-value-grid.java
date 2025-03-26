e659eccdd74e48d37eacfb28c547452d9416cde7
Abby <k.abhiditya09@gmail.com>
2025-03-26 14:31:28 +0530

    Time: 1081 ms (7.54%), Space: 78.5 MB (6.03%) - LeetHub

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int[] arr= new int[n*m];
+        int k=0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr[k++]=grid[i][j];
+            }
+        }
+        Arrays.sort(arr);
+        int mid=arr.length/2;
+        int result=0;
+        for(int i=0; i<mid; i++){
+            while(arr[i]<arr[mid]){
+                arr[i]+=x;
+                result++;
+            }
+        }
+        for(int i=mid+1; i<arr.length; i++){
+            while(arr[i]>arr[mid]){
+                arr[i]-=x;
+                result++;
+            }
+        }
+        for(int val: arr){
+            if(val!=arr[mid]){
+                return -1;
+            }
+        }
+        return result;
+    }
+}
