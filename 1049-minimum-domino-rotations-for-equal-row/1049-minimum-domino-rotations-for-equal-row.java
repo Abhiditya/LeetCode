f3046f7e705a338ee1588cf02113c0acbd1db5d6
Abby <k.abhiditya09@gmail.com>
2025-05-04 01:15:03 +0530

    Time: 4 ms (84.96%), Space: 50.4 MB (64.64%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
@@ -1,49 +1,25 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int n=tops.length;
-        int T=0, B=0;
-        int top=max(tops);
-        int bottom=max(bottoms);
-        for(int i=0; i<n; i++){
-            if(tops[i]!=top) {
-                if(bottoms[i]!=top){
-                    B=-1;
-                    break;
-                }
-                else{
-                    B++;
-                }
-            }
-        }
-        for(int i=0; i<n; i++){
-            if(bottoms[i]!=bottom) {
-                if(tops[i]!=bottom){
-                    T=-1;
-                    break;
-                }
-                else{
-                    T++;
-                }
-            }
-        }
-        if(B==-1 && T==-1)  return -1;
-        else if(B==-1 || T==-1) return  B > 0 ? B : T ;
-        else {
-            return Math.min(B,T);
-        }
+        int res1 = solve(tops, bottoms, tops[0]);
+        int res2 = (tops[0] == bottoms[0]) ? -1 : solve(tops, bottoms, bottoms[0]);
+
+        if(res1 == -1) return res2;
+        if(res2 == -1) return res1;
+        return Math.min(res1, res2);
     }
-    public int max(int[] nums){
-        HashMap<Integer, Integer> hash= new HashMap<>();
-        int max=0;
-        int max_val=nums[0];
-        for(int i=0; i<nums.length; i++){
-            int freq=hash.getOrDefault(nums[i],0)+1;
-            hash.put(nums[i],freq);
-            if(max<freq){
-                max=freq;
-                max_val=nums[i];
-            }
+    public int solve(int[] tops, int[] bottoms, int target) {
+        int T = 0;
+        int B= 0;
+        int n = tops.length;
+
+        for(int i = 0; i < n; i++) {
+            if(tops[i] != target && bottoms[i] != target)
+                return -1;
+            else if(tops[i] != target)
+                T++;
+            else if(bottoms[i] != target)
+                B++;
         }
-        return max_val;
+        return Math.min(T, B);
     }
 }
