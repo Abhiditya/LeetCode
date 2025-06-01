c6c61c4a64e223b557890f1c5fb60b41abf2cc68
Abby <k.abhiditya09@gmail.com>
2025-06-01 15:54:20 +0530

    Time: 1 ms (93.52%), Space: 40.8 MB (62.96%) - LeetHub

--- 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
+++ 3199-distribute-candies-among-children-i/3199-distribute-candies-among-children-i.java
@@ -1,12 +1,10 @@
 class Solution {
     public int distributeCandies(int n, int limit) {
         int count=0;
-        int a=0, b=0, c=0;
-        for(int i=0; i<=limit; i++){
-            for(int j=0; j<=limit; j++){
-                for(int k=0; k<=limit; k++){
-                    if(i+j+k==n) count++;
-                }
+        for(int i=0; i<=Math.min(limit,n); i++){
+            for(int j=0; j<=Math.min(limit,n-i); j++){
+                int k=n-i-j;
+                if(k>=0 && k<=limit) count++;
             }
         }
         return count;
