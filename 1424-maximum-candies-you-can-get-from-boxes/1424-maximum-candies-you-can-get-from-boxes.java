7c8592f1970629bcd0afd005f7a5be2e01b02547
Abby <k.abhiditya09@gmail.com>
2025-06-03 23:47:23 +0530

    Time: 10 ms (49.75%), Space: 59.6 MB (17.41%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.java
@@ -1,28 +1,33 @@
 class Solution {
     public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
-        int count=0;
-        int n=status.length;
+        int count = 0;
+        int n = status.length;
+
         boolean[] opened= new boolean[n];
-        boolean[] key_found= new boolean[n];
-        boolean[] box_found= new boolean[n];
+        boolean[] hasKey= new boolean[n];
+        boolean[] hasBox= new boolean[n];
+
         Queue<Integer> queue = new ArrayDeque<>();
+
         for(int i=0; i<initialBoxes.length; i++){
-            box_found[initialBoxes[i]]=true;
+            hasBox[initialBoxes[i]]=true;
             if(status[initialBoxes[i]]==1) queue.add(initialBoxes[i]);
         }
-        while(queue.peek()!=null){
-            int box=queue.poll();
-            if(!opened[box]){
-                count+=candies[box];
-                for(int i=0; i<containedBoxes[box].length; i++) {
-                    box_found[containedBoxes[box][i]]=true;
-                    if(key_found[containedBoxes[box][i]] || status[containedBoxes[box][i]]==1) queue.add(containedBoxes[box][i]);
-                }
-                for(int i=0; i<keys[box].length; i++) {
-                    key_found[keys[box][i]]=true;
-                    if(box_found[keys[box][i]]) queue.add(keys[box][i]);
-                }
-                opened[box]=true;
+
+        while(!queue.isEmpty()){
+            int box= queue.poll();
+            
+            if (opened[box]) continue;
+            opened[box]=true;
+            count += candies[box];
+            
+            for (int key : keys[box]) {
+                hasKey[key] = true;
+                if (hasBox[key] && !opened[key]) queue.offer(key);
+            }
+            for (int contained : containedBoxes[box]) {
+                hasBox[contained] = true;
+                if ((status[contained] == 1 || hasKey[contained]) && !opened[contained]) queue.offer(contained);
             }
         }
         return count;
