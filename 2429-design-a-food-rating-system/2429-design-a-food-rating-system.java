389170caaba9b13dfa5aba73038730e085c6dfbc
Abby <k.abhiditya09@gmail.com>
2025-09-17 21:58:52 +0530

    Time: 313 ms (13.33%), Space: 73.4 MB (76.67%) - LeetHub

---
+++ 2429-design-a-food-rating-system/2429-design-a-food-rating-system.java
@@ -0,0 +1,47 @@
+class FoodRatings {
+    Map<String, String> foodToCuisine;
+    Map<String, Integer> foodToRating;
+    Map<String, TreeSet<String>> cuisineToFoods;
+
+    public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+        foodToCuisine = new HashMap<>();
+        foodToRating = new HashMap<>();
+        cuisineToFoods = new HashMap<>();
+
+        for (int i = 0; i < foods.length; i++) {
+            String food = foods[i];
+            String cuisine = cuisines[i];
+            int rating = ratings[i];
+
+            foodToCuisine.put(food, cuisine);
+            foodToRating.put(food, rating);
+            cuisineToFoods.putIfAbsent(cuisine,new TreeSet<>((a,b)->{
+                int r1 = foodToRating.get(a);
+                int r2 = foodToRating.get(b);
+                if (r1 != r2) return r2 - r1;
+                return a.compareTo(b);
+            }));
+
+            cuisineToFoods.get(cuisine).add(food);
+        }
+    }
+
+    public void changeRating(String food, int newRating) {
+        String cuisine = foodToCuisine.get(food);
+        TreeSet<String> set = cuisineToFoods.get(cuisine);
+        set.remove(food);            
+        foodToRating.put(food, newRating);
+        set.add(food);
+    }
+    
+    public String highestRated(String cuisine) {
+        return cuisineToFoods.get(cuisine).first();
+    }
+}
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings obj = new FoodRatings(foods, cuisines, ratings);
+ * obj.changeRating(food,newRating);
+ * String param_2 = obj.highestRated(cuisine);
+ */
